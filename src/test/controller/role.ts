ee73e42b804d91f15cb9c464bc3a1f523d7b0d92
CherryBoom <huangxin8697@gmail.com>
2019-09-28 17:07:09 +0800

    feat:

--- src/test/controller/role.ts
+++ src/test/controller/role.ts
@@ -2,13 +2,13 @@ import Router from '../../decorators/Router'
 import Get from '../../decorators/Get'
 import Controller from '../../interface/Controller'
 @Router('/user')
-export default class RoleController extends Controller{
+export default class RoleController extends Controller {
   @Get('/login')
-  public login(){
+  public login() {
     return 'hello world'
   }
   @Get('/create')
-  public create(){
+  public create() {
     return this.s.role.getRole()
   }
 }
